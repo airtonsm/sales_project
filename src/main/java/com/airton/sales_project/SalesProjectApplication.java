ead132aa95e1a3e79102d9e69e3286c6a7957cce
airtonsm <airtonsmorais28@gmail.com>
2021-05-21 20:14:19 -0300

    initial project

---
+++ src/main/java/com/airton/sales_project/SalesProjectApplication.java
@@ -0,0 +1,15 @@
+package com.airton.sales_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesProjectApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SalesProjectApplication.class, args);
+
+    }
+
+}
