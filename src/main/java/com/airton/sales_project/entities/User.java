465c2a9b0a7d1a5c5a40e81be8923334fb192063
airtonsm <airtonsmorais28@gmail.com>
2021-05-21 21:25:18 -0300

    added anotation in User

--- src/main/java/com/airton/sales_project/entities/User.java
+++ src/main/java/com/airton/sales_project/entities/User.java
@@ -1,12 +1,17 @@
 package com.airton.sales_project.entities;
 
-import javax.annotation.processing.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
-
+@Entity
 public class User implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
