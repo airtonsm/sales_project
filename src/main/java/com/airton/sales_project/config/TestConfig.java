73e849b10a2dc7493a56e7fdea5f51b207c66a85
airtonsm <airtonsmorais28@gmail.com>
2021-05-23 19:11:06 -0300

    inject dependency

--- src/main/java/com/airton/sales_project/config/TestConfig.java
+++ src/main/java/com/airton/sales_project/config/TestConfig.java
@@ -1,9 +1,13 @@
 package com.airton.sales_project.config;
 
+import com.airton.sales_project.entities.Category;
 import com.airton.sales_project.entities.Order;
+import com.airton.sales_project.entities.Product;
 import com.airton.sales_project.entities.User;
 import com.airton.sales_project.entities.enums.OrderStatus;
+import com.airton.sales_project.repositories.CategoryRepository;
 import com.airton.sales_project.repositories.OrderRepository;
+import com.airton.sales_project.repositories.ProductRepository;
 import com.airton.sales_project.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -23,9 +27,28 @@ public class TestConfig implements CommandLineRunner { // class for population t
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
+
 
     @Override
     public void run(String... args) throws Exception { // método que executa  comandos quando é iniciado.
+
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+
+        Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+        Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+        Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+        Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+        Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+
+        categoryRepository.saveAll((Arrays.asList(cat1,cat2, cat3)));
+        productRepository.saveAll((Arrays.asList(p1,p2,p3,p4,p5)));
 
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988238383", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "9777777", "1231312");
@@ -34,9 +57,14 @@ public class TestConfig implements CommandLineRunner { // class for population t
         Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2, OrderStatus.WAITING_PAYMENT);
         Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1, OrderStatus.WAITING_PAYMENT);
 
+
         userRepository.saveAll(Arrays.asList(u1, u2)); // salvando users no DB
 
-       orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+        orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+
+
+
+
 
 
 
