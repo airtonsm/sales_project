5e51b37d25742019eef60f22300e67e4bce4a8b3
airtonsm <airtonsmorais28@gmail.com>
2021-05-25 22:04:09 -0300

    added subtotal orders

--- src/main/java/com/airton/sales_project/entities/Order.java
+++ src/main/java/com/airton/sales_project/entities/Order.java
@@ -36,6 +36,7 @@ public class Order implements Serializable {
     @OneToOne(mappedBy = "order", cascade = CascadeType.ALL) // mesmo id
     private Payment payment;
 
+
     public Order(){
 
     }
@@ -81,6 +82,7 @@ public class Order implements Serializable {
         }
     }
 
+
     public Payment getPayment() {
         return payment;
     }
@@ -91,6 +93,14 @@ public class Order implements Serializable {
 
     public Set<OrderItem> getItems(){
         return items;
+    }
+
+    public Double getTotal(){
+        double sum = 0.0;
+        for(OrderItem x : items){
+            sum += x.getSubTotal();
+        }
+        return sum;
     }
 
     @Override
